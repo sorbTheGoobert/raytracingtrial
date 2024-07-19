c8f4b2fad88df5c6fbc33f716e0914ba1b021423
= <=>
2024-07-19 10:23:34 +0800

    it was the vertical check

--- script.js
+++ script.js
@@ -120,44 +120,50 @@ function drawRays() {
         rayAngle -= 2 * Pi;
     }
     for(rayAmount = 0; rayAmount < 60; rayAmount++) {
-        dof = 0;
-        var disH = 1000000;
-        var hx = playerX, hy = playerY;
-        var angleTan = -1 / Math.tan(rayAngle);
-        if(rayAngle > Pi) {
-            // ry = ((Math.trunc(playerY)>>6)<<6) - 0.0001;
-            ry = ((Math.floor(playerY)>>6)<<6) - 0.0001;
-            rx = (playerY - ry) * angleTan + playerX;
-            yo = -64;
-            xo = -yo*angleTan;
-        }
-        if(rayAngle < Pi) {
-            // ry = ((Math.trunc(playerY)>>6)<<6) + 64;
-            ry = ((Math.floor(playerY)>>6)<<6) + 64;
-            rx = (playerY - ry) * angleTan + playerX;
-            yo = 64;
-            xo = -yo*angleTan;
-        }
-        if(rayAngle == 0 || rayAngle == Pi){
-            rx = playerX;
-            ry = playerY;
-            dof = 8;
-        }
-        while(dof < 8){
-            mx = Math.floor(rx) >> 6;
-            my = Math.floor(ry) >> 6;
-            mp = my * mapX + mx;
-            if(mp > 0 && mp < mapX*mapY && map[mp] == 1){
-                hx = rx;
-                hy = ry;
-                disH = dist(playerX, playerY, hx, hy, rayAngle);
-                dof = 8;
-            }else{
-                rx+=xo;
-                ry+=yo;
-                dof++;
-            }
-        }
+        // dof = 0;
+        // var disH = 0;
+        // var hx = playerX, hy = playerY;
+        // var angleTan = -1 / Math.tan(rayAngle);
+        // if(rayAngle > Pi) {
+        //     // ry = ((Math.trunc(playerY)>>6)<<6) - 0.0001;
+        //     ry = ((Math.floor(playerY)>>6)<<6) - 0.0001;
+        //     rx = (playerY - ry) * angleTan + playerX;
+        //     yo = -64;
+        //     xo = -yo*angleTan;
+        // }
+        // if(rayAngle < Pi) {
+        //     // ry = ((Math.trunc(playerY)>>6)<<6) + 64;
+        //     ry = ((Math.floor(playerY)>>6)<<6) + 64;
+        //     rx = (playerY - ry) * angleTan + playerX;
+        //     yo = 64;
+        //     xo = -yo*angleTan;
+        // }
+        // if(rayAngle == 0 || rayAngle == Pi){
+        //     rx = playerX;
+        //     ry = playerY;
+        //     dof = 8;
+        // }
+        // while(dof < 8){
+        //     mx = Math.floor(rx) >> 6;
+        //     my = Math.floor(ry) >> 6;
+        //     mp = my * mapX + mx;
+        //     if(mp > 0 && mp < mapX * mapY && map[mp] == 1){
+        //         hx = rx;
+        //         hy = ry;
+        //         disH = dist(playerX, playerY, hx, hy, rayAngle);
+        //         dof = 8;
+        //     }else{
+        //         rx+=xo;
+        //         ry+=yo;
+        //         dof++;
+        //     }
+        // }
+        // ctx.beginPath();
+        // ctx.strokeStyle = "red";
+        // ctx.lineWidth = 1;
+        // ctx.moveTo(playerX + 4.5, playerY + 4.5);
+        // ctx.lineTo(rx, ry);
+        // ctx.stroke();
         dof = 0;
         var nTan = -Math.tan(rayAngle);
         if(rayAngle > P2 && rayAngle < P3) {
@@ -192,38 +198,38 @@ function drawRays() {
                 dof++;
             }
         }
-        if(disV < disH) {
-            rx = vx;
-            ry = vy;
-            disT = disV;
-            ctx.strokeStyle = "rgb(255, 0, 0)"
-        }
-        if(disV > disH) {
-            rx = hx;
-            ry = hy;
-            disT = disH;
-            ctx.strokeStyle = "rgb(125, 0, 0)"
-        }
-        var ca = playerAngle - rayAngle;
-        if(ca < 0){
-            ca += 2 * Pi;
-        }
-        if(ca > 2 * Pi){
-            ca -= 2 * Pi;
-        }
-        disT = disT * Math.cos(ca);
-        var lineH = (mapSize * 320) / disT;
-        var lineO = 320 - lineH / 2;
-        if(lineH > 320){
-            lineH = 320;
-        }
+        // if(disV < disH) {
+        //     rx = vx;
+        //     ry = vy;
+        //     disT = disV;
+        //     ctx.strokeStyle = "rgb(0, 0, 255)"
+        // }
+        // if(disV > disH) {
+        //     rx = hx;
+        //     ry = hy;
+        //     disT = disH;
+        //     ctx.strokeStyle = "rgb(0, 255, 0)"
+        // }
+        // var ca = playerAngle - rayAngle;
+        // if(ca < 0){
+        //     ca += 2 * Pi;
+        // }
+        // if(ca > 2 * Pi){
+        //     ca -= 2 * Pi;
+        // }
+        // disT = disT * Math.cos(ca);
+        // var lineH = (mapSize * 320) / disT;
+        // var lineO = 320 - lineH / 2;
+        // if(lineH > 320){
+        //     lineH = 320;
+        // }
+        // ctx.beginPath();
+        // ctx.lineWidth = 8;
+        // ctx.moveTo(rayAmount*8+530, lineO);
+        // ctx.lineTo(rayAmount*8+530, lineH + lineO);
+        // ctx.stroke();
         ctx.beginPath();
-        ctx.lineWidth = 8;
-        ctx.moveTo(rayAmount*8+530, lineO);
-        ctx.lineTo(rayAmount*8+530, lineH + lineO);
-        ctx.stroke();
-        ctx.beginPath();
-        ctx.strokeStyle = "red";
+        ctx.strokeStyle = "blue";
         ctx.lineWidth = 1;
         ctx.moveTo(playerX + 4.5, playerY + 4.5);
         ctx.lineTo(rx, ry);
@@ -241,7 +247,7 @@ function drawRays() {
 }
 
 function redrawFrame() {
-    ctx.clearRect(0, 0, 1200, 600);
+    ctx.clearRect(0, 0, 1400, 600);
     drawMap();
     drawPlayer();
     drawRays();
