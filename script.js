7adc6e9f9773a37d19fc091d9fea39e5a307f5df
= <=>
2024-07-30 17:37:06 +0800

    i srsly have no idea what im doing now

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 const canvas = document.getElementById("canvas");
 const context = canvas.getContext("2d");
 
+
+
+
+
+
+
+
+
+
+// display
+
 const display = {
     map: [
         1, 1, 1, 1, 1, 1, 1, 1,
@@ -31,6 +42,15 @@ const display = {
     }
 }
 
+
+
+
+
+
+
+
+// player
+
 const PI = Math.PI;
 
 const player = {
@@ -41,8 +61,10 @@ const player = {
     deltay: 0,
     color: "yellow",
     angle: 0,
-    turnVelocity: 0.05,
+    turnVelocity: 0.085,
     pointerWidth: 3,
+    onMapX: undefined,
+    onMapY: undefined,
     draw: function () {
         context.fillStyle = this.color;
         context.fillRect(this.x, this.y, this.size, this.size);
@@ -60,10 +82,61 @@ const player = {
         sPressed: false,
         dPressed: false,
     },
+    castRays: function () {
+        let range = 0;
+        let xNearest, yNearest;
+        let xStep, yStep;
+        let rayAngle, rayAmount = 1;
+        let rayX = player.onMapX;
+        let rayY = player.onMapY;
+        rayAngle = this.angle;
+        // horizontal
+        for (i = 0; i < rayAmount; i++) {
+            let aTan = -1 / Math.tan(rayAngle);
+            if (rayAngle > PI) { // up
+                yNearest = ((Math.floor(this.y) >> 6) << 6) - 0.0000001;
+                xNearest = (this.y - yNearest) * aTan + this.x;
+                yStep = -64;
+                xStep = -yStep * aTan;
+            } else if (rayAngle < PI) { // down
+                yNearest = ((Math.floor(this.y) >> 6) << 6) + 64;
+                xNearest = (this.y - yNearest) * aTan + this.x;
+                yStep = 64;
+                xStep = -yStep * aTan;
+            } else if (rayAngle == PI || rayAngle == 0 || rayAngle == 2 * PI) { // left or right
+                range = 8;
+                xNearest = this.x;
+                yNearest = this.y;
+            }
+            while (range < 8) {
+                rayX = Math.floor(xNearest) >> 6;
+                rayY = Math.floor(yNearest) >> 6;
+                rayPos = rayY * display.horizontalCellAmount + rayX;
+                console.log(rayX + " rayX");
+                console.log(rayY + " rayY");
+                console.log(rayPos + " rayPost");
+                if (rayPos < display.horizontalCellAmount * display.verticalCellAmount && display.map[rayPos] == 1) {
+                    console.log("hit");
+                    range = 8; // hit wall
+                } else {
+                    xNearest += xStep;
+                    yNearest += yStep;
+                    range++;
+                }
+            }
+            context.beginPath();
+            context.lineWidth = 1;
+            context.strokeStyle = "green";
+            context.moveTo(this.x + this.size / 2, this.y + this.size / 2);
+            context.lineTo(xNearest, yNearest);
+            context.stroke();
+            context.closePath();
+        }
+    },
     update: function () {
         if (this.keys.wPressed) {
-            this.x += this.deltax;
-            this.y += this.deltay;
+            this.x += this.deltax * 3;
+            this.y += this.deltay * 3;
         }
         if (this.keys.aPressed) {
             this.angle -= this.turnVelocity;
@@ -72,8 +145,8 @@ const player = {
             }
         }
         if (this.keys.sPressed) {
-            this.x -= this.deltax;
-            this.y -= this.deltay;
+            this.x -= this.deltax * 3;
+            this.y -= this.deltay * 3;
         }
         if (this.keys.dPressed) {
             this.angle += this.turnVelocity;
@@ -83,8 +156,21 @@ const player = {
         }
         this.deltax = Math.cos(this.angle);
         this.deltay = Math.sin(this.angle);
+        this.onMapX = Math.floor(this.x / 100);
+        this.onMapY = Math.floor(this.y / 100);
     },
 }
+
+
+
+
+
+
+
+
+
+
+//movement
 
 addEventListener("keydown", function (event) {
     switch (event.code) {
@@ -119,12 +205,22 @@ addEventListener("keyup", function (event) {
     }
 })
 
+
+
+
+
+
+// initilize and update
+
+
 function init() {
     display.draw();
     canvas.width = display.cellSize * display.horizontalCellAmount;
     canvas.height = display.cellSize * display.verticalCellAmount;
     player.x = (display.cellSize * display.horizontalCellAmount - player.size) / 2,
     player.y = (display.cellSize * display.verticalCellAmount - player.size) / 2,
+    player.onMapX = Math.floor(player.x / 100);
+    player.onMapY = Math.floor(player.y / 100);
     player.draw();
     update();
 }
@@ -132,6 +228,7 @@ function init() {
 function update() {
     display.draw();
     player.update();
+    player.castRays();
     player.draw();
     requestAnimationFrame(update);
 }
