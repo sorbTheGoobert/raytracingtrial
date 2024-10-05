3e2546aa021786e08860645a65155dfb49e56875
sorbonaANOTHERDAMNWSL <reeldob34.com>
2024-10-05 15:54:28 +0800

    Yeah Im rewriting this. From the information i got from the internet and myself (oddly enough) made me realize that OOP was a bit too hard to organize so i decided to ditch OOP for this one. Assuming I actually do it this time

--- script.js
+++ script.js
@@ -1,236 +1,169 @@
-const canvas = document.getElementById("canvas");
-const context = canvas.getContext("2d");
 
+// * Canvas declorations
+const canvas = document.getElementById("canvas")
+const ctx = canvas.getContext("2d")
+const PI = Math.PI
 
+// * Map related
+const MAP = [
+    [1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1],
+];
 
-
-
-
-
-
-
-
-// display
-
-const display = {
-    map: [
-        1, 1, 1, 1, 1, 1, 1, 1,
-        1, 0, 0, 0, 0, 0, 0, 1,
-        1, 0, 1, 1, 0, 0, 0, 1,
-        1, 0, 1, 0, 0, 0, 0, 1,
-        1, 0, 0, 0, 0, 0, 0, 1,
-        1, 0, 0, 0, 0, 1, 0, 1,
-        1, 0, 0, 0, 0, 0, 0, 1,
-        1, 1, 1, 1, 1, 1, 1, 1,
-    ],
-    horizontalCellAmount: 8,
-    verticalCellAmount: 8,
-    cellSize: 100,
-    backGroundColor: "grey",
-    cellColor: "black",
-    draw: function () {
-        context.fillStyle = this.backGroundColor;
-        context.fillRect(0, 0, this.cellSize * this.horizontalCellAmount, this.cellSize * this.verticalCellAmount);
-        for (i = 0; i < this.verticalCellAmount; i++) {
-            for (l = 0; l < this.horizontalCellAmount; l++) {
-                if (this.map[i * 8 + l] === 1) {
-                    context.fillStyle = this.cellColor;
-                    context.fillRect(l * this.cellSize, i * this.cellSize, this.cellSize, this.cellSize);
-                }
-            }
-        }
-    }
+// * Player
+// Init
+let player_POS = {
+    x: 400,
+    y: 400,
 }
-
-
-
-
-
-
-
-
-// player
-
-const PI = Math.PI;
-
-const player = {
-    x: undefined,
-    y: undefined,
-    size: 15,
-    deltax: 0,
-    deltay: 0,
-    color: "yellow",
-    angle: 0,
-    turnVelocity: 0.085,
-    pointerWidth: 3,
-    onMapX: undefined,
-    onMapY: undefined,
-    draw: function () {
-        context.fillStyle = this.color;
-        context.fillRect(this.x, this.y, this.size, this.size);
-        context.beginPath();
-        context.lineWidth = this.pointerWidth;
-        context.strokeStyle = this.color;
-        context.moveTo(this.x + this.size / 2, this.y + this.size / 2);
-        context.lineTo(this.x + this.deltax * 30 + this.size / 2, this.y + this.deltay * 30 + this.size / 2);
-        context.stroke();
-        context.closePath();
-    },
-    keys: {
-        wPressed: false,
-        aPressed: false,
-        sPressed: false,
-        dPressed: false,
-    },
-    castRays: function () {
-        let range = 0;
-        let xNearest, yNearest;
-        let xStep, yStep;
-        let rayAngle, rayAmount = 1;
-        let rayX = player.onMapX;
-        let rayY = player.onMapY;
-        rayAngle = this.angle;
-        // horizontal
-        for (i = 0; i < rayAmount; i++) {
-            let aTan = -1 / Math.tan(rayAngle);
-            if (rayAngle > PI) { // up
-                yNearest = ((Math.floor(this.y) >> 6) << 6) - 0.0000001;
-                xNearest = (this.y - yNearest) * aTan + this.x;
-                yStep = -64;
-                xStep = -yStep * aTan;
-            } else if (rayAngle < PI) { // down
-                yNearest = ((Math.floor(this.y) >> 6) << 6) + 64;
-                xNearest = (this.y - yNearest) * aTan + this.x;
-                yStep = 64;
-                xStep = -yStep * aTan;
-            } else if (rayAngle == PI || rayAngle == 0 || rayAngle == 2 * PI) { // left or right
-                range = 8;
-                xNearest = this.x;
-                yNearest = this.y;
-            }
-            while (range < 8) {
-                rayX = Math.floor(xNearest) >> 6;
-                rayY = Math.floor(yNearest) >> 6;
-                rayPos = rayY * display.horizontalCellAmount + rayX;
-                console.log(rayX + " rayX");
-                console.log(rayY + " rayY");
-                console.log(rayPos + " rayPost");
-                if (rayPos < display.horizontalCellAmount * display.verticalCellAmount && display.map[rayPos] == 1) {
-                    console.log("hit");
-                    range = 8; // hit wall
-                } else {
-                    xNearest += xStep;
-                    yNearest += yStep;
-                    range++;
-                }
-            }
-            context.beginPath();
-            context.lineWidth = 1;
-            context.strokeStyle = "green";
-            context.moveTo(this.x + this.size / 2, this.y + this.size / 2);
-            context.lineTo(xNearest, yNearest);
-            context.stroke();
-            context.closePath();
-        }
-    },
-    update: function () {
-        if (this.keys.wPressed) {
-            this.x += this.deltax * 3;
-            this.y += this.deltay * 3;
-        }
-        if (this.keys.aPressed) {
-            this.angle -= this.turnVelocity;
-            if (this.angle < 0) {
-                this.angle += 2 * PI;
-            }
-        }
-        if (this.keys.sPressed) {
-            this.x -= this.deltax * 3;
-            this.y -= this.deltay * 3;
-        }
-        if (this.keys.dPressed) {
-            this.angle += this.turnVelocity;
-            if (this.angle > 2 * PI) {
-                this.angle -= 2 * PI;
-            }
-        }
-        this.deltax = Math.cos(this.angle);
-        this.deltay = Math.sin(this.angle);
-        this.onMapX = Math.floor(this.x / 100);
-        this.onMapY = Math.floor(this.y / 100);
-    },
+const player_size = 15
+const player_color = "yellow";
+const player_speed = 2;
+const player_turnspeed = 0.05;
+let player_angle = 0;
+const player_keys = {
+    w: false,
+    a: false,
+    s: false,
+    d: false,
+    left: false,
+    right: false,
 }
-
-
-
-
-
-
-
-
-
-
-//movement
-
-addEventListener("keydown", function (event) {
-    switch (event.code) {
+window.addEventListener("keydown", (e) => {
+    switch (e.code) {
         case "KeyW":
-            player.keys.wPressed = true;
+            player_keys.w = true;
             break;
         case "KeyA":
-            player.keys.aPressed = true;
+            player_keys.a = true;
             break;
         case "KeyS":
-            player.keys.sPressed = true;
+            player_keys.s = true;
             break;
         case "KeyD":
-            player.keys.dPressed = true;
+            player_keys.d = true;
             break;
-    }
-});
-addEventListener("keyup", function (event) {
-    switch (event.code) {
-        case "KeyW":
-            player.keys.wPressed = false;
+        case "ArrowLeft":
+            player_keys.left = true;
             break;
-        case "KeyA":
-            player.keys.aPressed = false;
-            break;
-        case "KeyS":
-            player.keys.sPressed = false;
-            break;
-        case "KeyD":
-            player.keys.dPressed = false;
+        case "ArrowRight":
+            player_keys.right = true;
             break;
     }
 })
-
-
-
-
-
-
-// initilize and update
-
-
-function init() {
-    display.draw();
-    canvas.width = display.cellSize * display.horizontalCellAmount;
-    canvas.height = display.cellSize * display.verticalCellAmount;
-    player.x = (display.cellSize * display.horizontalCellAmount - player.size) / 2,
-    player.y = (display.cellSize * display.verticalCellAmount - player.size) / 2,
-    player.onMapX = Math.floor(player.x / 100);
-    player.onMapY = Math.floor(player.y / 100);
-    player.draw();
-    update();
+window.addEventListener("keyup", (e) => {
+    switch (e.code) {
+        case "KeyW":
+            player_keys.w = false;
+            break;
+        case "KeyA":
+            player_keys.a = false;
+            break;
+        case "KeyS":
+            player_keys.s = false;
+            break;
+        case "KeyD":
+            player_keys.d = false;
+            break;
+        case "ArrowLeft":
+            player_keys.left = false;
+            break;
+        case "ArrowRight":
+            player_keys.right = false;
+            break;
+    }
+})
+// Functions / Methods
+const player_update = () => {
+    if (player_keys.w) {
+        player_POS.x += player_speed * Math.cos(player_angle);
+        player_POS.y += player_speed * Math.sin(player_angle);
+    }
+    if (player_keys.a) {
+        let tempAngle = player_angle - 0.5 * PI;
+        if (tempAngle < 0) {
+            tempAngle += 2 * PI;
+        }
+        player_POS.x += player_speed * Math.cos(tempAngle);
+        player_POS.y += player_speed * Math.sin(tempAngle);
+    }
+    if (player_keys.s) {
+        player_POS.x -= player_speed * Math.cos(player_angle);
+        player_POS.y -= player_speed * Math.sin(player_angle);
+    }
+    if (player_keys.d) {
+        let tempAngle = player_angle + 0.5 * PI;
+        if (tempAngle > 2 * PI) {
+            tempAngle -= 2 * PI;
+        }
+        player_POS.x += player_speed * Math.cos(tempAngle);
+        player_POS.y += player_speed * Math.sin(tempAngle);
+    }
+    if (player_keys.left) {
+        player_angle -= player_turnspeed;
+    }
+    if (player_keys.right) {
+        player_angle += player_turnspeed;
+    }
+    if (player_angle > 2 * PI) {
+        player_angle -= 2 * PI
+    }
+    if (player_angle < 0) {
+        player_angle += 2 * PI
+    }
 }
 
-function update() {
-    display.draw();
-    player.update();
-    player.castRays();
-    player.draw();
-    requestAnimationFrame(update);
+// * Drawing methods
+const drawPlayerPointer = () => {
+    ctx.strokeStyle = player_color;
+    ctx.lineWidth = 3
+    ctx.beginPath()
+    ctx.moveTo(player_POS.x, player_POS.y);
+    ctx.lineTo(player_POS.x + Math.cos(player_angle) * 25, player_POS.y + Math.sin(player_angle) * 25)
+    ctx.stroke()
+}
+const drawPlayer = () => {
+    drawPlayerPointer()
+    ctx.fillStyle = player_color;
+    ctx.fillRect(player_POS.x - player_size / 2, player_POS.y - player_size / 2, player_size, player_size);
+}
+const drawMap = () => {
+    for (let row = 0; row < MAP.length; row++) {
+        for (let col = 0; col < MAP[row].length; col++) {
+            if (MAP[row][col] == 1) {
+                ctx.fillStyle = "black";
+                ctx.fillRect(row * 100, col * 100, 100, 100);
+            }
+        }
+    }
+}
+const clearCanvas = () => {
+    ctx.clearRect(0, 0, 800, 800);
+    ctx.fillStyle = "grey";
+    ctx.fillRect(0, 0, 800, 800);
+}
+const draw1Frame = () => {
+    clearCanvas();
+    drawMap();
+    drawPlayer();
 }
 
-init();
+// * Init()
+const init = () => {
+    draw1Frame();
+    setInterval(requestAnimationFrame, 1000 / 60, update);
+}
+
+// * Update()
+const update = () => {
+    player_update();
+    draw1Frame();
+}
+
+window.onload = init;
